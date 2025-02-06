a19f61e5ea2adad689a39db644dc7d599b732f87
Nayan Mehta <nayanm2403@gmail.com>
2025-02-06 11:36:36 +0530

    css changes

--- reactjs/React-api/src/ud_components/BookForm.jsx
+++ reactjs/React-api/src/ud_components/BookForm.jsx
@@ -5,27 +5,27 @@ function BookForm({HandleSubmit,title,setTitle,setAuthor,author,editMode}) {
     return (
         <>
             <form onSubmit={HandleSubmit} className="form-container">
-                <div className='input-container'>
-                <div className='inputs'>
-                    <Input
-                    type="text"
-                    onChange={(e) => setTitle(e.target.value)}
-                    placeholder="Enter Book Title"
-                    value={title}
-                    required
-                    className="input-field"
-                    />
-                    <Input
-                    type="text"
-                    onChange={(e) => setAuthor(e.target.value)}
-                    placeholder="Enter Book Author"
-                    value={author}
-                    required
-                    className="input-field"
-                    />
-                </div>
+                {/* <div className='input-container'> */}
+                    <div className='inputs'>
+                        <Input
+                        type="text"
+                        onChange={(e) => setTitle(e.target.value)}
+                        placeholder="Enter Book Title"
+                        value={title}
+                        required
+                        className="input-field"
+                        />
+                        <Input
+                        type="text"
+                        onChange={(e) => setAuthor(e.target.value)}
+                        placeholder="Enter Book Author"
+                        value={author}
+                        required
+                        className="input-field"
+                        />
+                    </div>
                 <Button type="submit" className="add-button">{editMode ? 'Update' : 'Add'}</Button>
-                </div>
+                {/* </div>x */}
             </form>
         </>
     )
