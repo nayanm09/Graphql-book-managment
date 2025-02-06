6aeee5147d7210481cde217cc50f3d59755198c3
Nayan Mehta <nayanm2403@gmail.com>
2025-02-06 12:03:16 +0530

    final style changes

--- reactjs/React-api/src/ud_components/BookForm.jsx
+++ reactjs/React-api/src/ud_components/BookForm.jsx
@@ -1,34 +1,32 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-
-function BookForm({HandleSubmit,title,setTitle,setAuthor,author,editMode}) {
+import './BookForm.css';
+function BookForm({ HandleSubmit, title, setTitle, setAuthor, author, editMode }) {
     return (
         <>
             <form onSubmit={HandleSubmit} className="form-container">
-                {/* <div className='input-container'> */}
-                    <div className='inputs'>
-                        <Input
+                <div className="inputs">
+                    <Input
                         type="text"
                         onChange={(e) => setTitle(e.target.value)}
                         placeholder="Enter Book Title"
                         value={title}
                         required
                         className="input-field"
-                        />
-                        <Input
+                    />
+                    <Input
                         type="text"
                         onChange={(e) => setAuthor(e.target.value)}
                         placeholder="Enter Book Author"
                         value={author}
                         required
                         className="input-field"
-                        />
-                    </div>
+                    />
+                </div>
                 <Button type="submit" className="add-button">{editMode ? 'Update' : 'Add'}</Button>
-                {/* </div>x */}
             </form>
         </>
-    )
+    );
 }
 
-export default BookForm
+export default BookForm;
